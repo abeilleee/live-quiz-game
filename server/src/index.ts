fcfcd77b3777aaa15c10022cde493878b2db3ff0
abeilleee <abeilleee@users.noreply.github.com>
2026-04-20 17:10:36 +0300

    feat: add WS starting msg

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import { WebSocketServer } from 'ws';
+import { Logger } from './utils/Logger';
 
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // WebSocket server
 const wss = new WebSocketServer({ port: PORT });
+Logger.success(`🚀 Websocker server started on ${PORT} port`);
