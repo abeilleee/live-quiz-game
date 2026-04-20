f8253a052bedb3ed9f8cfc941e06f144ccd03535
abeilleee <abeilleee@users.noreply.github.com>
2026-04-20 17:10:09 +0300

    feat: add colored logger

---
+++ server/src/utils/Logger.ts
@@ -0,0 +1,19 @@
+import chalk from "chalk";
+
+export class Logger {
+  private static plainColor = '#ffffff';
+  private static errorColor = '#f40136';
+  private static successColor = '#0bf7f7';
+
+  static plain(text: string) {
+    console.log(chalk.hex(this.plainColor)(text));
+  }
+
+  static error(text: string) {
+    console.log(chalk.hex(this.errorColor).bold(text));
+  }
+
+  static success(text: string) {
+    console.log(chalk.hex(this.successColor).bold(text));
+  }
+}
